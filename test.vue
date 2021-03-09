44e09d804953415259cc324331275e244f6d279b
n335833 <n335833@Gmail.com>
2021-03-09 15:12:48 +0800

    Create test.vue

---
+++ test.vue
@@ -0,0 +1,10 @@
+var sheetUrl =
+  "https://spreadsheets.google.com/feeds/list/103jMpae8cO-caBf1n1hYWMaVC6wWZ6J_OT_QbhWyywA/1/public/values?alt=json";
+
+new Vue({
+  el: "#app",
+  data: {
+    test: "[123,223]",
+    entries: null
+  })
+  
